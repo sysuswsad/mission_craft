afacc65f95febc218980daf01d87a793cb35f380
penglsh <1728343423@qq.com>
2019-05-25 22:16:20 +0800

    message page v1 commit

---
+++ missioncraft-project/src/views/MessagePage.vue
@@ -0,0 +1,172 @@
+<template xmlns:v-slot="http://www.w3.org/1999/XSL/Transform">
+  <div>
+    <el-card class="message-card">
+      <template v-slot:header>
+        <el-button class="el-icon-arrow-left" v-on:click="toLastPage" size="mini"></el-button>
+        <el-button class="el-icon-delete-solid" type="danger" size="mini" v-on:click="deleteRow()" v-bind:disabled="multipleSelection.length === 0"></el-button>
+        <el-button size="mini" v-on:click="markRead()" v-bind:disabled="multipleSelection.length === 0">标为已读</el-button>
+        <el-button size="mini" v-on:click="unMarkRead()" v-bind:disabled="multipleSelection.length === 0">标为未读</el-button>
+        <el-button size="mini" v-on:click="cancelSelection()" v-bind:disabled="multipleSelection.length === 0">取消选择</el-button>
+        <el-pagination id="pagination-container" background layout="prev, pager, next, sizes, total, jumper"
+                       v-bind:page-sizes="[5, 10, 20, 30]"
+                       v-bind:page-size="pageSize"
+                       v-bind:total="tableData.length"
+                       v-on:current-change="handleCurrentChange"
+                       v-on:size-change="handleSizeChange">
+        </el-pagination>
+      </template>
+      <div id="table-container">
+        <el-table id="message-table" ref="multipleTable" stripe
+                  v-bind:data="tableData.slice((currentPage-1) * pageSize, currentPage * pageSize)"
+                  v-bind:default-sort = "{prop: 'date', order: 'descending'}"
+                  v-on:selection-change="handleSelectionChange">
+          <el-table-column type="selection" width="55"></el-table-column>
+          <el-table-column prop="hasRead" label="已读" sortable width="80"></el-table-column>
+          <el-table-column prop="date" label="日期" sortable width="150"></el-table-column>
+          <el-table-column prop="content" label="内容" show-overflow-tooltip></el-table-column>
+        </el-table>
+      </div>
+    </el-card>
+    <el-dialog
+        title="确认删除所选信件?"
+        :visible.sync="dialogVisible"
+        width="30%">
+      <span slot="footer" class="dialog-footer">
+        <el-button v-on:click="dialogVisible = false">取 消</el-button>
+        <el-button type="primary" v-on:click="onConfirmDelete()">确 定</el-button>
+      </span>
+    </el-dialog>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'MessagePage',
+  data () {
+    return {
+      tableData: [
+        {
+          hasRead: '✔',
+          date: '2016-05-02',
+          content: '您有新的任务。'
+        },
+        {
+          hasRead: '',
+          date: '2016-05-04',
+          content: 'xxx任务发布成功。'
+        },
+        {
+          hasRead: '✔',
+          date: '2016-05-01',
+          content: '你发布的xxx任务已完成，请确认。'
+        },
+        {
+          hasRead: '',
+          date: '2016-05-03',
+          content: '您接收的xxx任务已完成，闲钱已到账。'
+        },
+        {
+          hasRead: '',
+          date: '2016-05-04',
+          content: '您接收的xxx任务已完成，闲钱已到账。'
+        },
+        {
+          hasRead: '',
+          date: '2016-05-06',
+          content: '您接收的xxx任务已完成，闲钱已到账。'
+        },
+        {
+          hasRead: '',
+          date: '2016-05-05',
+          content: '您接收的xxx任务已完成，闲钱已到账。'
+        },
+        {
+          hasRead: '',
+          date: '2016-05-07',
+          content: '您接收的xxx任务已完成，闲钱已到账。'
+        },
+        {
+          hasRead: '',
+          date: '2016-05-08',
+          content: '您接收的xxx任务已完成，闲钱已到账。'
+        }
+      ],
+      multipleSelection: [],
+      dialogVisible: false,
+      pageSize: 5,
+      currentPage: 1
+    }
+  },
+  methods: {
+    toLastPage () {
+      this.$router.back()
+    },
+    handleSelectionChange (val) {
+      this.multipleSelection = val
+    },
+    markRead () {
+      // to-do: refresh data in db
+
+      for (let i = 0; i < this.multipleSelection.length; ++i) {
+        this.multipleSelection[i].hasRead = '✔'
+      }
+      // clear selection after operations
+      this.$refs.multipleTable.clearSelection()
+    },
+    unMarkRead () {
+      // to-do: refresh data in db
+
+      for (let i = 0; i < this.multipleSelection.length; ++i) {
+        this.multipleSelection[i].hasRead = ''
+      }
+      // clear selection after operations
+      this.$refs.multipleTable.clearSelection()
+    },
+    deleteRow () {
+      // dialog if continue the delete operation
+      if (this.multipleSelection.length === 0) {
+        return
+      }
+      this.dialogVisible = true
+    },
+    onConfirmDelete () {
+      // to-do: refresh data in db
+
+      for (let i = 0; i < this.multipleSelection.length; ++i) {
+        let index = this.tableData.indexOf(this.multipleSelection[i])
+        this.tableData.splice(index, 1)
+      }
+      this.dialogVisible = false
+      console.log('delete')
+    },
+    cancelSelection () {
+      // clear selection after operations
+      this.$refs.multipleTable.clearSelection()
+    },
+    handleCurrentChange (cPage) {
+      this.currentPage = cPage
+    },
+    handleSizeChange (pSize) {
+      this.pageSize = pSize
+    }
+  }
+}
+</script>
+
+<style scoped>
+  .message-card {
+    width: 100%;
+  }
+
+  #table-container {
+    width: 100%;
+  }
+
+  #message-table {
+    width: 100%;
+  }
+
+  #pagination-container {
+    float: right;
+  }
+</style>
