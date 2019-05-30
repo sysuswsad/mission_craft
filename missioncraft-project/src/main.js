4d3ce2ed2ca748694fdde970c898036b8b20fe43
AzureSky <gg424463241@gmail.com>
2019-05-30 12:52:13 +0800

    create square page

--- missioncraft-project/src/main.js
+++ missioncraft-project/src/main.js
@@ -5,11 +5,13 @@ import store from './store'
 import ElementUI from 'element-ui'
 import './theme/element/index.css'
 import * as VueCookies from 'vue-cookies'
+import vueWaterfallEasy from 'vue-waterfall-easy'
 
 import './filters'
 
 Vue.use(ElementUI)
 Vue.use(VueCookies)
+Vue.use(vueWaterfallEasy)
 
 Vue.config.productionTip = false
 
