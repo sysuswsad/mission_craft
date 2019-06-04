4f61cfbd45678197f3a9c6f7fd06911663f853b9
penglsh <1728343423@qq.com>
2019-06-04 13:42:28 +0800

    我的发布和我的领取的初步UI

---
+++ missioncraft-project/src/views/PublicationPage.vue
@@ -0,0 +1,165 @@
+<template>
+  <div id="publication-container">
+    <el-card>
+      <template v-slot:header>
+        <el-tabs v-model="activeTab" v-on:tab-click="handleClick">
+          <el-tab-pane label="进行中" name="processing"></el-tab-pane>
+          <el-tab-pane label="已完成" name="complete"></el-tab-pane>
+        </el-tabs>
+        <el-pagination
+            id="pagination-container" background layout="prev, pager, next, sizes, total, jumper"
+            v-bind:page-sizes="[5, 10, 20, 30]"
+            v-bind:page-size="pageSize"
+            v-bind:current-page.sync="currentPage"
+            v-bind:total="tableMission.length"
+            v-on:current-change="handleCurrentChange"
+            v-on:size-change="handleSizeChange">
+        </el-pagination>
+      </template>
+      <el-table id="mission-table"
+                stripe
+                v-bind:data="tableMission.slice((currentPage-1) * pageSize, currentPage * pageSize)">
+        <el-table-column label="任务类型"
+                         prop="missionType"
+                         v-bind:filters="[{text:'问卷调查', value: ''}, {text: '其他任务', value: '✔'}]"
+                         v-bind:filter-method="filtersHandler"></el-table-column>
+        <el-table-column prop="status" label="任务摘要"></el-table-column>
+        <el-table-column align="right" v-if="activeTab === 'processing'">
+          <template v-slot:default="scope">
+            <el-button
+                size="mini"
+                type="danger"
+                v-on:click="handleDelete(scope.$index, scope.row)">取消任务</el-button>
+          </template>
+        </el-table-column>
+      </el-table>
+    </el-card>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'PublicationPage',
+  data () {
+    return {
+      activeTab: 'processing',
+      allMission: [
+        {
+          id: '1',
+          missionType: '问卷调查',
+          status: '已完成'
+        },
+        {
+          id: '2',
+          missionType: '其他任务',
+          status: '已完成'
+        },
+        {
+          id: '3',
+          missionType: '其他任务',
+          status: '已完成'
+        },
+        {
+          id: '4',
+          missionType: '问卷调查',
+          status: '进行中'
+        },
+        {
+          id: '5',
+          missionType: '其他任务',
+          status: '已完成'
+        },
+        {
+          id: '6',
+          missionType: '问卷调查',
+          status: '已完成'
+        },
+        {
+          id: '7',
+          missionType: '其他任务',
+          status: '已完成'
+        }
+      ],
+      pageSize: 5,
+      currentPage: 1,
+      tableMission: []
+    }
+  },
+
+  created: function () {
+    if (this.tableMission.length !== 0) {
+      this.tableMission = []
+    }
+
+    for (let i = 0; i < this.allMission.length; ++i) {
+      if (this.allMission[i].status === '进行中') {
+        this.tableMission.push(this.allMission[i])
+      }
+    }
+  },
+
+  methods: {
+    handleClick (tab, event) {
+      if (this.tableMission.length !== 0) {
+        this.tableMission = []
+      }
+
+      for (let i = 0; i < this.allMission.length; ++i) {
+        if (tab.name === 'processing' && this.allMission[i].status === '进行中') {
+          this.tableMission.push(this.allMission[i])
+        } else if (tab.name === 'complete' && this.allMission[i].status === '已完成') {
+          this.tableMission.push(this.allMission[i])
+        }
+      }
+    },
+
+    handleCurrentChange (cPage) {
+      this.currentPage = cPage
+    },
+
+    handleSizeChange (pSize) {
+      this.pageSize = pSize
+    },
+
+    filtersHandler (value, row, column) {
+      const property = column['property']
+      return row[property] === value
+    },
+
+    handleDelete (index, row) {
+      this.$confirm('确认取消该任务？', '提示', {
+        confirmButtonText: '确定',
+        cancelButtonText: '取消',
+        type: 'warning'
+      }).then(() => {
+        this.onConfirmDelete(index)
+        this.$message({
+          type: 'success',
+          message: '任务取消成功！'
+        })
+      })
+    },
+
+    onConfirmDelete (i) {
+      // to-do: refresh data in db
+      let index = this.allMission.indexOf(this.tableMission[i])
+      this.allMission.splice(index, 1)
+      this.tableMission.splice(i, 1)
+    }
+  }
+}
+</script>
+
+<style scoped>
+  #publication-container {
+    margin: 0 10px 0 10px;
+  }
+
+  #pagination-container {
+    float: right;
+  }
+
+  #mission-table {
+    cursor: pointer;
+  }
+</style>
