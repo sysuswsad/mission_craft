49fb8b0ca1a15ed0c6de02cb3d26d575fbe13081
潘鉴_163401766 <gitee.com/panjianone>
2019-06-04 20:54:30 +0800

    测试上传文件URL，通过

--- missioncraft-project/app/api/user.py
+++ missioncraft-project/app/api/user.py
@@ -241,9 +241,12 @@ def update_info():
 def get_avatar_url():
     db = get_db()
     avatar = db.execute(
-        'SELECT avatar FROM User WHERE idUser = ?', (g.user['idUser'])
+        'SELECT avatar FROM User WHERE idUser = ?', (g.user['idUser'],)
     ).fetchone()
-    return ok('Get user avatar successfully', data={'avatar':avatar})
+    if avatar['avatar'] is not None:
+        return ok('Get user avatar successfully', data={'avatar':avatar['avatar']})      
+    else:
+        return bad_request('User avatar is not available')
 
 
 
@@ -255,9 +258,11 @@ def allowed_file(filename):
 
 @bp.route('/image/<filename>')
 def get_uploaded_file(filename):
+
     return send_from_directory(current_app.config['UPLOAD_FOLDER'],
                                filename)
 
+import tempfile
 @bp.route('/avatar/', methods=['POST'])
 @auth.login_required
 def change_avatar():
@@ -266,9 +271,8 @@ def change_avatar():
     if file and allowed_file(file.filename):
         filename = file.filename
         extention = filename.rsplit('.', 1)[1]
+
         file.save(os.path.join(current_app.config['UPLOAD_FOLDER'], str(g.user['idUser']) + '.' + extention))
-        file.seek(0)
-        
         
         avatar = os.path.join(current_app.config['BASE_STATIC_URL'], str(g.user['idUser']) + '.' + extention)
         db = get_db()
