816726130654b207cad892e8c17fa7ad9d917168
penglsh <1728343423@qq.com>
2019-06-09 23:30:48 +0800

    弹框查看其它任务（非问卷）详情

--- missioncraft-project/src/views/PublicationPage.vue
+++ missioncraft-project/src/views/PublicationPage.vue
@@ -21,7 +21,8 @@
       <el-table
         id="mission-table"
         stripe
-        v-bind:data="tableMission.slice((currentPage-1) * pageSize, currentPage * pageSize)">
+        v-bind:data="tableMission.slice((currentPage-1) * pageSize, currentPage * pageSize)"
+        v-on:row-click="rowClick">
         <el-table-column
           label="任务类型"
           prop="missionType"
@@ -40,6 +41,56 @@
         </el-table-column>
       </el-table>
     </el-card>
+    <el-dialog width="70%"
+               center
+               v-bind:visible.sync="dialogVisible"
+               v-bind:before-close="handleClose">
+      <template v-slot:title>
+        <h2>{{ missionTitle }}</h2>
+      </template>
+      <el-divider></el-divider>
+      <div id="detail-container">
+        <h1>发布者</h1>
+        <el-col v-bind:span="8" v-bind:offset="10">
+          <img id="img-container" v-bind:src=url alt="加载失败" v-on:click="showInfo"/>
+          <div id="username-container">caixukun</div>
+        </el-col>
+        <div v-for="(index, value) in contactWay" v-bind:key="value">
+          <el-row v-if="contactWay[value] !== emptyStr" id="contact-container">
+            <el-col v-bind:span="8" v-bind:offset="9" v-if="value === 'phone'">
+              <span>{{ '手机号码：' + contactWay[value] }}</span>
+            </el-col>
+            <el-col v-bind:span="8" v-bind:offset="9" v-if="value === 'weChat'">
+              <span>{{ '微信：' + contactWay[value] }}</span>
+            </el-col>
+            <el-col v-bind:span="8" v-bind:offset="9" v-if="value === 'qq'">
+              <span>{{ 'qq：' + contactWay[value] }}</span>
+            </el-col>
+            <el-col v-bind:span="8" v-bind:offset="9" v-if="value === 'other'">
+              <span>{{ '其他联系方式：' + contactWay[value] }}</span>
+            </el-col>
+          </el-row>
+        </div>
+        <el-divider></el-divider>
+        <h1>任务详情</h1>
+        <el-row>
+          <el-col v-bind:span="20" v-bind:offset="2" style="background-color: #2a98dd">caixukun...</el-col>
+        </el-row>
+        <el-row v-bind:gutter="15" style="margin-top: 30px">
+          <el-col v-bind:span="5" style="padding: 8px 0 0 40px">{{ startTime }}</el-col>
+          <el-col v-bind:span="14">
+            <el-slider v-bind:step="Math.floor(100 / timeDiff(startTime, endTime))"
+                       v-bind:value="passTime(startTime)"
+                       v-bind:format-tooltip="formatTooltip"
+                       disabled></el-slider>
+          </el-col>
+          <el-col v-bind:span="5" style="padding: 8px 0 0 0">{{ endTime }}</el-col>
+        </el-row>
+      </div>
+      <span slot="footer" class="dialog-footer">
+        <el-button v-on:click="dialogVisible = false">关 闭</el-button>
+      </span>
+    </el-dialog>
   </div>
 </template>
 
@@ -53,42 +104,61 @@ export default {
         {
           id: '1',
           missionType: '问卷调查',
+          missionTitle: '大学生就业调查',
           status: '已完成'
         },
         {
           id: '2',
           missionType: '其他任务',
+          missionTitle: '快递代取',
           status: '已完成'
         },
         {
           id: '3',
           missionType: '其他任务',
+          missionTitle: '大学英语线下辅导',
           status: '已完成'
         },
         {
           id: '4',
           missionType: '问卷调查',
+          missionTitle: '第三饭堂饭菜调查',
           status: '进行中'
         },
         {
           id: '5',
           missionType: '其他任务',
+          missionTitle: '篮球租赁请求',
           status: '已完成'
         },
         {
           id: '6',
           missionType: '问卷调查',
+          missionTitle: '大学生就业调查',
           status: '已完成'
         },
         {
           id: '7',
           missionType: '其他任务',
+          missionTitle: '网上求夸找自信',
           status: '已完成'
         }
       ],
       pageSize: 5,
       currentPage: 1,
-      tableMission: []
+      tableMission: [],
+      dialogVisible: false,
+      missionTitle: '',
+      url: 'https://oj.vmatrix.org.cn/img/default-avatar.b6541da3.png',
+      contactWay: {
+        phone: '17878787878',
+        weChat: 'we-chat',
+        qq: '',
+        other: ''
+      },
+      emptyStr: '',
+      startTime: '2019-06-09 00:00:00',
+      endTime: '2019-06-10 23:59:59'
     }
   },
 
@@ -151,6 +221,74 @@ export default {
       let index = this.allMission.indexOf(this.tableMission[i])
       this.allMission.splice(index, 1)
       this.tableMission.splice(i, 1)
+    },
+
+    rowClick (row) {
+      // judge and jump to the detail page
+      if (row.missionType === '问卷调查') {
+        // to-do: route to detail page and pass some parameters to sign if the mission is over/finished or
+        // if the mission is published by the one clicking the row
+
+      } else {
+        // dialog for other missions
+        this.missionTitle = row.missionTitle
+        this.dialogVisible = true
+      }
+    },
+
+    showInfo () {
+      console.log('show info')
+    },
+
+    formatTooltip () {
+      let startTime = Date.now()
+      let endTime = new Date(this.endTime)
+      let left = '剩余0天0时0分0秒'
+      if (endTime.getTime() > startTime) {
+        let msDiff = endTime.getTime() - startTime
+        // compute day left
+        let leftDay = Math.floor(msDiff / (1000 * 24 * 60 * 60))
+        // hours left after computing day left
+        let leaveForHour = msDiff % (1000 * 24 * 60 * 60)
+        // compute hour left
+        let leftHour = Math.floor(leaveForHour / (1000 * 60 * 60))
+        let leaveForMinute = leaveForHour % (1000 * 3600)
+        let leftMinute = Math.floor(leaveForMinute / (1000 * 60))
+        let leaveForSecond = leaveForMinute % (1000 * 60)
+        let leftSecond = Math.round(leaveForSecond / 1000)
+        left = '剩余' + leftDay + '天' + leftHour + '时' + leftMinute + '分' + leftSecond + '秒'
+      }
+      return left
+    },
+
+    timeDiff (sTime, eTime) {
+      let startTime = new Date(sTime)
+      let endTime = new Date(eTime)
+      let leftHour = 0
+      if (endTime.getTime() > startTime.getTime()) {
+        let msDiff = endTime.getTime() - startTime.getTime()
+        leftHour = Math.floor(msDiff / (1000 * 3600))
+      }
+      return leftHour
+    },
+
+    passTime (startTime) {
+      let nowTime = Date.now()
+      let sTime = new Date(startTime)
+      let passHour = 0
+      if (nowTime > sTime.getTime()) {
+        let msDiff = nowTime - sTime.getTime()
+        passHour = Math.ceil(msDiff / (1000 * 3600))
+      }
+      return passHour * Math.floor(100 / this.$options.methods.timeDiff(startTime, this.endTime))
+    },
+
+    handleClose(done) {
+      this.$confirm('确认关闭？')
+        .then(_ => {
+          done()
+        })
+        .catch(_ => {})
     }
   }
 }
@@ -167,5 +305,25 @@ export default {
 
   #mission-table {
     cursor: pointer;
+  }
+
+  #detail-container {
+    margin: 0 0px 0 0px;
+  }
+
+  #img-container {
+    width: 100px;
+    height: 100px;
+    border-radius: 50%;
+    cursor: pointer;
+  }
+
+  #username-container {
+    margin: 0 0 10px 15px;
+    font-weight: bold;
+  }
+
+  #contact-container {
+    margin-bottom: 10px;
   }
 </style>
